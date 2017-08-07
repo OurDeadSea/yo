2896c7dc5b7d87ef14dde4cfd8ca9274a3fef623
Eduardo Acuña Yeomans <eduardo.acye@gmail.com>
2017-08-07 00:32:45 -0700

    Add basic RTM websocket support

--- src/yo/slack/rtm.clj
+++ src/yo/slack/rtm.clj
@@ -1,5 +1,49 @@
 (ns yo.slack.rtm
-  (:require [yo.slack.core :as slack]))
+  (:require [yo.slack.core :as slack]
+            [clojure.core.async :as async :refer [chan go >! <! >!! <!! go-loop close!]]
+            [clojure.data.json :as json]
+            [gniazdo.core :as ws]))
+
+(defn connect-into-websocket [url incoming]
+  (ws/connect url
+    :on-connect (fn [session] :TODO)
+    :on-binary (fn [payload offset length] :TODO)
+    :on-error (fn [throwable] :TODO)
+    :on-close (fn [status-code reason] :TODO)
+    :on-receive (fn [event]
+                  (go (>! incoming (json/read-str event :key-fn keyword))))))
+
+(defn- transmit-onto-websocket [ws-client outgoing]
+  (go-loop []
+    (let [event (<! outgoing)]
+      (when event
+        (if (= event :close)
+          (ws/close ws-client)
+          (ws/send-msg ws-client (json/write-str event)))
+        (recur)))))
+
+(defn establish [conn incoming outgoing]
+  (when (:ok conn)
+    (let [ws-client (connect-into-websocket (:url conn) incoming)]
+      (transmit-onto-websocket ws-client outgoing))))
+
+(comment
+
+  (def incoming (chan))
+  (def outgoing (chan))
+
+  (go-loop []
+    (let [event (<! incoming)]
+      (when event
+        (clojure.pprint/pprint event)
+        (recur))))
+
+  (establish (connect "<<bot token goes here>>") incoming outgoing)
+
+  (>!! outgoing {:type "ping"})
+  (>!! outgoing :close)
+
+  )
 
 (defn connect
   "This method begins a Real Time Messaging API session and reserves your application a specific URL with which to connect via websocket.
